4429915eb4d0f443f4795d6778b62da54b5441b3
Meshack <meshack@example.com>
2025-12-20 09:07:50 +0300

    Vercel fix: add app = application to wsgi.py

--- xrp_site/wsgi.py
+++ xrp_site/wsgi.py
@@ -1,4 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','xrp_site.settings')
+
 application=get_wsgi_application()
+app = application  # For Vercel Python runtime compatibility
